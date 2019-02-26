846b416c1c2fa116fa9b6c7048d060bcec71e78d
OrionHand <37989810+OrionHand@users.noreply.github.com>
2019-02-26 15:47:17 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(function() {
 
     $('#total').show();
     $('.navegacion a:first').addClass('activo');
+
     $('.navegacion a').on('click', function() {
 
         $('.navegacion a').removeClass('activo');
@@ -11,8 +12,35 @@ $(function() {
 
         var enlace = $(this).attr('href');
         $(enlace).fadeIn(500);
+
         return false;
     });
+
+    $('#total2').show();
+    $('.navegacion2 a:first').addClass('activo');
+
+    $('.navegacion2 a').on('click', function() {
+
+        $('.navegacion2 a').removeClass('activo');
+        $(this).addClass('activo');
+
+        $('.ocultar2').hide();
+
+        var enlace = $(this).attr('href');
+        $(enlace).fadeIn(500);
+
+        return false;
+    });
+
+    $('.ventana-hy-vee .customer a').on('click', function() {
+
+
+        $('.hy-vee').slideToggle("slow");
+
+    });
+
+
+
 });
 
 $(function() {
@@ -30,6 +58,7 @@ $(function() {
         var enlace = $(this).attr('href');
         $(enlace).fadeOut(500);
     });
+
 
 
 });
